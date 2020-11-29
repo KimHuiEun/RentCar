dc5cca871e32c065d7f6615f476dc8dd23f611b3
장석 <jangsuk701@gmail.com>
2020-11-29 23:23:24 +0900

    Main소스 깔끔하게

--- CarRentalManagementSystem/RentCar/Main.Designer.cs
+++ CarRentalManagementSystem/RentCar/Main.Designer.cs
@@ -44,16 +44,16 @@ namespace RentCar
             this.ReturnDate = new System.Windows.Forms.DateTimePicker();
             this.label5 = new System.Windows.Forms.Label();
             this.btnSrchData = new System.Windows.Forms.Button();
+            this.BtnMyinfo = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // BtnLogin
             // 
             this.BtnLogin.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnLogin.Location = new System.Drawing.Point(560, 42);
-            this.BtnLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnLogin.Location = new System.Drawing.Point(392, 28);
             this.BtnLogin.Name = "BtnLogin";
-            this.BtnLogin.Size = new System.Drawing.Size(188, 49);
+            this.BtnLogin.Size = new System.Drawing.Size(132, 33);
             this.BtnLogin.TabIndex = 0;
             this.BtnLogin.Text = "로그인";
             this.BtnLogin.UseVisualStyleBackColor = true;
@@ -62,10 +62,9 @@ namespace RentCar
             // BtnCreate
             // 
             this.BtnCreate.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnCreate.Location = new System.Drawing.Point(767, 42);
-            this.BtnCreate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnCreate.Location = new System.Drawing.Point(537, 28);
             this.BtnCreate.Name = "BtnCreate";
-            this.BtnCreate.Size = new System.Drawing.Size(188, 49);
+            this.BtnCreate.Size = new System.Drawing.Size(132, 33);
             this.BtnCreate.TabIndex = 1;
             this.BtnCreate.Text = "회원가입";
             this.BtnCreate.UseVisualStyleBackColor = true;
@@ -74,10 +73,9 @@ namespace RentCar
             // BtnAdminLogin
             // 
             this.BtnAdminLogin.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnAdminLogin.Location = new System.Drawing.Point(22, 488);
-            this.BtnAdminLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnAdminLogin.Location = new System.Drawing.Point(15, 325);
             this.BtnAdminLogin.Name = "BtnAdminLogin";
-            this.BtnAdminLogin.Size = new System.Drawing.Size(189, 50);
+            this.BtnAdminLogin.Size = new System.Drawing.Size(132, 33);
             this.BtnAdminLogin.TabIndex = 2;
             this.BtnAdminLogin.Text = "관리자로그인";
             this.BtnAdminLogin.UseVisualStyleBackColor = true;
@@ -91,10 +89,9 @@ namespace RentCar
             "지점1",
             "지점2",
             "지점3"});
-            this.CbLocation.Location = new System.Drawing.Point(707, 185);
-            this.CbLocation.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.CbLocation.Location = new System.Drawing.Point(495, 123);
             this.CbLocation.Name = "CbLocation";
-            this.CbLocation.Size = new System.Drawing.Size(248, 41);
+            this.CbLocation.Size = new System.Drawing.Size(175, 30);
             this.CbLocation.TabIndex = 3;
             this.CbLocation.Visible = false;
             // 
@@ -102,10 +99,9 @@ namespace RentCar
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(576, 188);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(403, 125);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(61, 33);
+            this.label1.Size = new System.Drawing.Size(40, 22);
             this.label1.TabIndex = 4;
             this.label1.Text = "지점";
             this.label1.Visible = false;
@@ -114,10 +110,9 @@ namespace RentCar
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(576, 254);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(403, 169);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(99, 33);
+            this.label2.Size = new System.Drawing.Size(65, 22);
             this.label2.TabIndex = 6;
             this.label2.Text = "차 크기";
             this.label2.Visible = false;
@@ -133,10 +128,9 @@ namespace RentCar
             "중형",
             "준대형",
             "대형"});
-            this.Cbsize.Location = new System.Drawing.Point(707, 251);
-            this.Cbsize.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Cbsize.Location = new System.Drawing.Point(495, 167);
             this.Cbsize.Name = "Cbsize";
-            this.Cbsize.Size = new System.Drawing.Size(248, 41);
+            this.Cbsize.Size = new System.Drawing.Size(175, 30);
             this.Cbsize.TabIndex = 5;
             this.Cbsize.Visible = false;
             // 
@@ -144,10 +138,9 @@ namespace RentCar
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(576, 312);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(403, 208);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(107, 33);
+            this.label3.Size = new System.Drawing.Size(70, 22);
             this.label3.TabIndex = 7;
             this.label3.Text = "대여일자";
             this.label3.Visible = false;
@@ -156,10 +149,9 @@ namespace RentCar
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Consolas", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(576, 365);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(403, 243);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(107, 33);
+            this.label4.Size = new System.Drawing.Size(70, 22);
             this.label4.TabIndex = 13;
             this.label4.Text = "반납일자";
             this.label4.Visible = false;
@@ -167,10 +159,9 @@ namespace RentCar
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(22, 113);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(15, 75);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(500, 362);
+            this.pictureBox1.Size = new System.Drawing.Size(350, 241);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 17;
             this.pictureBox1.TabStop = false;
@@ -178,19 +169,17 @@ namespace RentCar
             // RentDate
             // 
             this.RentDate.CalendarFont = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RentDate.Location = new System.Drawing.Point(707, 315);
-            this.RentDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.RentDate.Location = new System.Drawing.Point(495, 210);
             this.RentDate.Name = "RentDate";
-            this.RentDate.Size = new System.Drawing.Size(249, 28);
+            this.RentDate.Size = new System.Drawing.Size(175, 21);
             this.RentDate.TabIndex = 18;
             this.RentDate.Visible = false;
             // 
             // ReturnDate
             // 
-            this.ReturnDate.Location = new System.Drawing.Point(707, 368);
-            this.ReturnDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ReturnDate.Location = new System.Drawing.Point(495, 245);
             this.ReturnDate.Name = "ReturnDate";
-            this.ReturnDate.Size = new System.Drawing.Size(249, 28);
+            this.ReturnDate.Size = new System.Drawing.Size(175, 21);
             this.ReturnDate.TabIndex = 19;
             this.ReturnDate.Visible = false;
             // 
@@ -198,30 +187,39 @@ namespace RentCar
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Consolas", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(30, 28);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(21, 19);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(375, 61);
+            this.label5.Size = new System.Drawing.Size(246, 41);
             this.label5.TabIndex = 20;
             this.label5.Text = "RentCar Page";
             // 
             // btnSrchData
             // 
-            this.btnSrchData.Location = new System.Drawing.Point(836, 435);
-            this.btnSrchData.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSrchData.Location = new System.Drawing.Point(585, 290);
             this.btnSrchData.Name = "btnSrchData";
-            this.btnSrchData.Size = new System.Drawing.Size(120, 46);
+            this.btnSrchData.Size = new System.Drawing.Size(84, 31);
             this.btnSrchData.TabIndex = 21;
             this.btnSrchData.Text = "검색";
             this.btnSrchData.UseVisualStyleBackColor = true;
             this.btnSrchData.Visible = false;
             this.btnSrchData.Click += new System.EventHandler(this.btnSrchData_Click);
             // 
+            // BtnMyinfo
+            // 
+            this.BtnMyinfo.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnMyinfo.Location = new System.Drawing.Point(538, 67);
+            this.BtnMyinfo.Name = "BtnMyinfo";
+            this.BtnMyinfo.Size = new System.Drawing.Size(132, 32);
+            this.BtnMyinfo.TabIndex = 22;
+            this.BtnMyinfo.Text = "내 정보";
+            this.BtnMyinfo.UseVisualStyleBackColor = true;
+            // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1000, 551);
+            this.ClientSize = new System.Drawing.Size(700, 367);
+            this.Controls.Add(this.BtnMyinfo);
             this.Controls.Add(this.btnSrchData);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.ReturnDate);
@@ -236,7 +234,6 @@ namespace RentCar
             this.Controls.Add(this.BtnAdminLogin);
             this.Controls.Add(this.BtnCreate);
             this.Controls.Add(this.BtnLogin);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Main";
             this.Text = "RentCar";
             this.Load += new System.EventHandler(this.Main_Load);
@@ -262,5 +259,6 @@ namespace RentCar
         private System.Windows.Forms.DateTimePicker ReturnDate;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button btnSrchData;
+        private System.Windows.Forms.Button BtnMyinfo;
     }
 }
