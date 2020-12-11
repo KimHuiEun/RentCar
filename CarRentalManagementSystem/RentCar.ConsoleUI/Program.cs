38c598ceb1ba7c315187e508c9142cc7ac5f9dc5
KEEHY <63379191+KEEHY@users.noreply.github.com>
2020-12-11 13:28:29 +0900

    새 프로젝트 ConsoleUI 추가.

---
+++ CarRentalManagementSystem/RentCar.ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using RentCar.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentCar.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RentCarEntities context = new RentCarEntities();
+            int count = context.Users.Count();
+            Console.WriteLine(count);
+
+        }
+    }
+}
--- CarRentalManagementSystem/RentCar.ConsoleUI/Properties/AssemblyInfo.cs
+++ CarRentalManagementSystem/RentCar.ConsoleUI/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("RentCar")]
+[assembly: AssemblyTitle("RentCar.ConsoleUI")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RentCar")]
+[assembly: AssemblyProduct("RentCar.ConsoleUI")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("83925919-376a-4716-8def-d165629ed666")]
+[assembly: Guid("b47e7adc-f8f9-4252-8e74-0006678566e7")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
